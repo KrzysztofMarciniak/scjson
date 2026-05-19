c515d3920f9d769f9c141a4635f7c9c4bb1ccfd8
KrzysztofMarciniak <96542207+KrzysztofMarciniak@users.noreply.github.com>
2026-05-19 15:50:06 +0000

    error handling

--- internal/scj_map_init.c
+++ internal/scj_map_init.c
@@ -5,14 +5,17 @@
 
 #include "scj_error.h"
 #include "scj_struct.h"
+
 #define SCJ_MAP_INITIAL_CAPACITY 16
 
-void scj_map_init(scjson self) {
+scj_error_info scj_map_init(scjson self) {
         self->value.object.map.capacity = SCJ_MAP_INITIAL_CAPACITY;
         self->value.object.map.count    = 0;
         self->value.object.map.buckets =
             calloc(SCJ_MAP_INITIAL_CAPACITY, sizeof(scj_node*));
         if (!self->value.object.map.buckets) {
-                scj_err_set(SCJ_ERR_ALLOC, scj_error_string(SCJ_ERR_ALLOC));
+                return scj_err_set(SCJ_ERR_ALLOC,
+                                   "scj_map_init.c: mem calloc failed.");
         }
+        return scj_err_ok();
 }
--- internal/scj_map_init.h
+++ internal/scj_map_init.h
@@ -2,7 +2,8 @@
 #ifndef SCJ_MAP_INIT_H
 #define SCJ_MAP_INIT_H
 #include "../scjson.h"
+#include "scj_error.h"
 
-void scj_map_init(scjson self);
+scj_error_info scj_map_init(scjson self);
 
 #endif
